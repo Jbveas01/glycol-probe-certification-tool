5de594f67dca004b67d01ecd4b718b0aa81685cf
Jacob Veasey <Jbveas01@gmail.com>
2022-03-26 18:34:20 -0400

    Added heroku support

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Probe = require("./models/probe");
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(express.static("build"));
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 app.use(cors());
